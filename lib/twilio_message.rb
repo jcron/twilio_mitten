b41323dbfb1fe58964b0ae442a32d8f037eb5a66
Jeremy <jeremykentc@gmail.com>
2013-10-30 08:53:50 -0400

    more tweaking of the message

--- lib/twilio_message.rb
+++ lib/twilio_message.rb
@@ -10,36 +10,44 @@ class TwilioMessage
   end
 
   def forward_sms
-    send_twilio_sms
+    to_number = @params[:To]
+    from_number = @params[:From]
+    if authenticate(from_number)
+      client_code = lookup_client to_number
+      send_twilio_sms client_code, from_number unless client_code.nil?
+    end
   end
 
   def voice_reject_message
-    twilio_response = Twilio::TwiML::Response.new do |response|
+    Twilio::TwiML::Response.new do |response|
       response.Reject :reason => "busy"
     end.text
-    twilio_response
   end
 
   private
 
-  def send_twilio_sms
-    sms_body = "#{@params[:Body]}"
-    @settings.to_numbers.each do |to_number|
-      @client.account.messages.create(:body => sms_body, :to => to_number, :from => @settings.sms_from_number)
+  def authenticate from_number
+    @client.account.incoming_phone_numbers.list.each do |number|
+      if number.phone_number == from_number
+        return true
+      end
     end
   end
 
-  def get_sms_messages message
-    messages = []
-    while message.length > 0
-      if message.length > 160
-        messages << message[0..159]
-        message = message[160..message.length]
-      else
-        messages << message
-        break
+  def send_twilio_sms client_code, from_number
+    sms_body = "#{client_code}: #{@params[:Body]}"
+    @settings.to_numbers.each do |to_number|
+      log "#{to_number}: #{from_number} #{sms_body}"
+      @client.account.messages.create(:body => sms_body, :to => to_number, :from => from_number)
+    end
+  end
+
+  def lookup_client from_number
+    @settings.client_mapping.each do |client|
+      if client["client_number"] == from_number
+        return client["client_code"]
       end
     end
-    messages
+    nil
   end
 end
